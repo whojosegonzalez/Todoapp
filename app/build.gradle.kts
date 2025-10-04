4210f59e005cdfbea2ccd433a58578b1e7e5dea3
jmg <who.jose.gonzalez@gmail.com>
2025-10-03 22:51:20 -0700

    Set Up the Project with Android Studio

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.todoapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.todoapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+}
